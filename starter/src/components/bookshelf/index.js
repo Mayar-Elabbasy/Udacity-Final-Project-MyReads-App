5a3519e91a398cbc6dd71c113085aae44fa95a8c
Mayar-Elabbasy <mayarelabbasy2016@gmail.com>
2022-07-23 16:45:09 +0200

    feat/Add props type checking for the bookshelf component

--- starter/src/components/bookshelf/index.js
+++ starter/src/components/bookshelf/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import BookshelfBooks from '../bookshelf-books';
 import BookshelfTitle from '../bookshelf-title';
+import PropTypes from 'prop-types';
 
 function Bookshelf({ bookshelfTitle, books, onChangeBookshelf }) {
     return (
@@ -9,6 +10,13 @@ function Bookshelf({ bookshelfTitle, books, onChangeBookshelf }) {
             <BookshelfBooks books={books} onChangeBookshelf={onChangeBookshelf} />
         </div>
     )
+}
+
+// Add component props type checking
+Bookshelf.propTypes = {
+    bookshelfTitle: PropTypes.string.isRequired,
+    books: PropTypes.array.isRequired,
+    onChangeBookshelf: PropTypes.func.isRequired,
 }
 
 export default Bookshelf;
